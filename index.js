634839bd63980d120fbdf87887cb8e132fb164ba
sanary-62 <uzmanisafat@gmail.com>
2025-06-14 17:37:11 +0600

    added database data

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-// ✅ middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sanary.5oi2id1.mongodb.net/?retryWrites=true&w=majority&appName=Sanary`;
 
-// MongoDB client setup
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,10 +24,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    
+
+
+const itemsCollection = client.db ('WhereIsIt').collection('items');
+
+
+
+app.get ('/items', async(req,res) => {
+    const cursor = itemsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+
+app.get('/items/:id', async(req,res) =>{
+         const id = req.params.id;
+         const query ={ _id: new ObjectId(id)}
+         const result =await itemsCollection.findOne(query);
+         res.send(result)
+})
+
+
+app.post('/items', async (req, res) => {
+  try {
+    const newItem = req.body;
+    const result = await itemsCollection.insertOne(newItem);
+    res.status(201).send({ success: true, insertedId: result.insertedId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ success: false, message: 'Failed to add item' });
+  }
+});
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // await client.close(); // Keep it commented if you want to use the client
+    // await client.close(); 
   }
 }
 run().catch(console.dir);
@@ -39,3 +74,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`WhereIsIt server is running on port ${port}`);
 });
+
+
+
